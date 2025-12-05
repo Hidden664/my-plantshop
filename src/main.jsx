2f934d3349117863056e839e96d6f84a96354f8c
Your Name <you@example.com>
2025-12-05 16:16:43 +0500

    Fix landing visibility: CSS z-index + HashRouter for GH Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import store from './store/store';
-import './index.css';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import store from "./store/store";
+import "./index.css";
+import "./App.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
