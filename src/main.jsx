8eadb85ed06fe8c719da926efdd032a0c8b21e54
Your Name <you@example.com>
2025-12-05 16:20:53 +0500

    Fix GH Pages: HashRouter + vite base + landing CSS z-index

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-import store from "./store/store";
-import "./index.css";
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom'; // <-- use HashRouter
+import App from './App';
+import store from './store/store';
+import './index.css';
+import './App.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <HashRouter>
